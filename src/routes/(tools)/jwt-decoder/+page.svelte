e09fc0ae5bc38a7642e430352fdd953d6a0c0fe1
Jujhar Singh Sandhu <sjujhar532@gmail.com>
2024-07-05 20:22:45 +0530

    jwt-decoder functionality added
    A decodeJWT function to decode a JSON Web Token (JWT) and display its payload.

---
+++ src/routes/(tools)/jwt-decoder/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    import { writable } from "svelte/store";
+    let inputJWT = "";
+    let decodedJWT: any = null;
+    let errorMessage = writable("");
+  
+    function decodeJWT() {
+      try {
+        const [, payloadBase64] = inputJWT.split(".");
+        const decodedPayload = JSON.parse(atob(payloadBase64));
+  
+        decodedJWT = decodedPayload;
+        errorMessage.set("");
+      } catch (error) {
+        decodedJWT = null;
+        errorMessage.set("Error decoding JWT. Please check your input.");
+      }
+    }
+  
+    function clearFields() {
+      
+    }
+  </script>
+  
+  
